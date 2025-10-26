3a62ff064f1e8612cd2ba77de35da3aca8440655
PumukyDev <adrian.bertosgomez@gmail.com>
2025-10-26 19:12:07 +0100

    refactor: Reorganize project folder structure

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = "";
-$dbname = "";
-$username = "";
-$password = "";
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_DATABASE') ;
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
